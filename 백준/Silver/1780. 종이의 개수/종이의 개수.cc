6f6d555a3a157ddd0937635eedc570fdd39838db
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-07 11:45:36 +0900

    Update 종이의 개수.cc

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -6,7 +6,7 @@ int N;
 int row_dir[3] = {0,1,2};
 int col_dir[3] = {0,1,2};
 int result[3];
-bool compare(int start_row,int start_col,int N) {
+bool compare(int start_row,int start_col,int N) { //처음엔 solve내에 전부다 써주었으나, 비교하는 부분만 함수로 빼주면 코드가 좀더 간결해짐. 그러나 성능면에선 별차이 없었음 8ms차이
     int value = arr[start_row][start_col];
     for (int i=start_row ; i<start_row +N ; i++) {
         for (int j=start_col; j<start_col+N ; j++) {
@@ -39,7 +39,7 @@ void solve(int start_row,int start_col, int N) {
 }
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); // 이두분을 넣느냐 마느냐에 따라 600ms나 차이가 났음. 귀찮다고 생각하지 말고 무조건 넣는게 좋을듯. (900ms->300ms
     cin >> N;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -51,4 +51,5 @@ int main() {
         cout << result[i];
         cout <<'\n';
     }
+
 }
