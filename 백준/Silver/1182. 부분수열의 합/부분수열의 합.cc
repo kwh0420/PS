64f453b1ee89d5788132cb562bd3bef94ef37fd0
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-08 14:27:22 +0900

    Update 부분수열의 합.cc

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -7,12 +7,12 @@ int N;
 int S;
 int cnt=0;
 void queen(int row,int sum) {
-    if (row == N) {
+    if (row == N) {// base condition, 이거 없으면 무한루프 
         if (sum== S)
              cnt++;
         return;}
     else {
-        queen(row+1,sum);
+        queen(row+1,sum); //어차피 무조건 다음 row를 방문하므로 visited배열써서 백트래킹해줄 필요가 없음.
         queen(row+1,sum+arr[row]);
     }
 }
@@ -24,6 +24,7 @@ int main() {
         cin >> arr[i];
     }
     queen(0,0);
-    if (S==0) cnt--;
+    if (S==0) cnt--; //공집합처리
     cout << cnt;
+
 }
