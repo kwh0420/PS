1263e4940331c48eea6b0139df2c037d3e5875f6
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-09 12:28:50 +0900

    [Silver III] Title: N과 M (7), Time: 392 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,M;
+int arr[10];
+int input_arr[10];
+int vis[10];
+void N_M(int depth,int n) {
+    if (depth == M) {
+        for (int i=0; i<M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i=0; i<N; i++) {
+
+        arr[depth] = input_arr[i];
+        N_M(depth+1,i);
+    }
+}
+int main () {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M;
+    for (int i=0; i<N; i++) {
+        cin >> input_arr[i];
+    }
+    sort (input_arr,input_arr+N);
+    N_M(0,0);
+}
