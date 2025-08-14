d06f8ccb696fb5b5aa20f213b61a230f7d4e1793
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-14 11:47:53 +0900

    Update 로봇 청소기.cc

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -26,7 +26,7 @@ int main() {
         re=0;
         int vision =d;
         for (int i=0; i<4; i++) {
-            vision=(vision-1+4)%4;
+            vision=(vision-1+4)%4; // 회전을 언제할지에 따라 답이 달라짐
             int nrow=row+row_dir[vision];
             int ncol=col+col_dir[vision];
             if (0<=nrow&&nrow<N&&0<=ncol&&ncol<M&&vis[nrow][ncol]==0&&arr[nrow][ncol]==0) {
@@ -38,6 +38,7 @@ int main() {
                 d=vision;
                 break;
             }
+            //원래는 회전 코드가 여기 있었음
         }
         if (re==1) {
             continue;
@@ -58,3 +59,4 @@ int main() {
     }
     cout << cnt;
 }
+
