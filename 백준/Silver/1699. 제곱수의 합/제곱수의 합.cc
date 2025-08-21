430d064f1a6051538461450e6a7beb7932f4aea7
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-21 10:02:01 +0900

    Update 제곱수의 합.cc

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -10,9 +10,10 @@ int main() {
     for (int i = 1; i <= N; i++) {
         dp[i]=i;
         for (int j=1; j*j<=i; j++) {
-            dp[i]=min(dp[i], dp[i-j*j]+1);
+            dp[i]=min(dp[i], dp[i-j*j]+1); // i보다 작은 모든 제곱수를 고려해야함. d[i]=[i]로 초기값설정, 처음엔 i보다 처음으로 작은 수만 고려했음.
         }
     }
     cout << dp[N];
+
 
 }
