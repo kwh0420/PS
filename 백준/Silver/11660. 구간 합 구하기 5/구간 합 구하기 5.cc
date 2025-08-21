b6e68dfe14f1c25f16079196c5a62f34db70db64
김우현 <143983378+kwh0420@users.noreply.github.com>
2025-08-21 10:52:15 +0900

    Update 구간 합 구하기 5.cc

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -12,18 +12,18 @@ int main() {
     cin >> N;
     int M;
     cin >> M;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 1; i <= N; i++) { // (1,1)부터 입력을 받고 가장자리는 0으로 채운다면 케이스를 불필요하게 나눌 필요가 없음
+        for (int j = 1; j <= N; j++) {
             cin >> dp[i][j];
         }
     }
-    for (int i = 1; i < N; i++) {
-        dp[i][0] += dp[i - 1][0];
-        dp[0][i] += dp[0][i - 1];
+    for (int i = 2; i <= N; i++) {
+        dp[i][1] += dp[i - 1][1];
+        dp[1][i] += dp[1][i - 1];
     }
 
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
+    for (int i = 2; i <= N; i++) {
+        for (int j = 2; j <= N; j++) {
             dp[i][j] += dp[i][j - 1] + dp[i - 1][j]-dp[i-1][j-1];
         }
     }
@@ -32,15 +32,8 @@ int main() {
         int x1, x2, y1, y2;
         cin >> x1 >> y1;
         cin >> x2 >> y2;
-        if (x1==1) {
-            cout << dp[x2-1][y2-1]-dp[x2-1][y1-2] <<'\n';
-        }
-        else if (y1==0) {
-            cout << dp[x2-1][y2-1]-dp[x1-2][y2-1] << '\n';
 
-        }
-        else {
-            cout << dp[x2-1][y2-1]+dp[x1-2][y1-2]-dp[x1-2][y2-1]-dp[x2-1][y1-2]<<'\n';
-        }
+            cout << dp[x2][y2]+dp[x1-1][y1-1]-dp[x1-1][y2]-dp[x2][y1-1]<<'\n';
+
     }
 }
