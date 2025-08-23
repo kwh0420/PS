c137cae0ce95c6159b193e69fe320dcf7d89ccf2
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-08-23 10:49:02 +0900

    [Gold IV] Title: 트리 노드 합의 최댓값, Time: 48 ms, Memory: 13656 KB -BaekjoonHub

---
+++ 백준/Gold/25515. 트리 노드 합의 최댓값/트리 노드 합의 최댓값.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+long long w[100002];
+vector<long long> v[100002];
+long long dp[100002];
+long long go(long long cur,long long par) {
+    if (v[cur].size()==1 && cur !=0) {
+            return w[cur];
+    }
+    long long sum=0;
+    for (long long i : v[cur]) {
+        if (i== par) {
+            continue;
+        }
+        long long result = go(i,cur);
+        if (result<0) {
+            continue;
+        }
+        sum +=result;
+    }
+    return dp[cur]=sum+w[cur];
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for (long long i=0; i<n-1; i++) {
+        long long a,b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for (long long i=0; i<n; i++) {
+        cin>>w[i];
+    }
+
+    cout << go(0,-1);
+    cout << "\n";
+}
