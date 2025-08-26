84dfeb4d8a122dc1d914311b7271764ac93fdc6e
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-08-26 13:56:31 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<long long int> plusarr;
+vector<long long int> minusarr;
+long long int valueofzero;
+
+ int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long int n;
+    cin >> n;
+    for (long long int i = 0; i < n; i++) {
+        long long int a;
+        cin >> a;
+        if (a>0) {
+            plusarr.push_back(a);
+        }
+        else if (a<0) {
+            minusarr.push_back(a);
+        }
+        else {
+            valueofzero=1;
+        }
+    }
+    long long int sum=0;
+    sort(plusarr.begin(), plusarr.end(), greater<long long int>());
+    sort(minusarr.begin(), minusarr.end());
+
+    long long int idx=0;
+    while (idx<plusarr.size()) {
+        if (idx==plusarr.size()-1) {
+            sum+=plusarr[idx];
+            break;
+        }
+        else if (plusarr[idx]==1 || plusarr[idx+1]==1) {
+            sum+=plusarr[idx];
+            idx+=1;
+        }
+        else {
+            sum+=plusarr[idx]*plusarr[idx+1];
+            idx+=2;
+        }
+    }
+     idx=0;
+    while (idx<minusarr.size()) {
+        if (idx==minusarr.size()-1) {
+            if (valueofzero==1) {
+                break;
+            }
+            sum+=minusarr[idx];
+        }
+        else {
+            sum+=minusarr[idx]*minusarr[idx+1];
+        }
+        idx+=2;
+    }
+     cout << sum;
+}
