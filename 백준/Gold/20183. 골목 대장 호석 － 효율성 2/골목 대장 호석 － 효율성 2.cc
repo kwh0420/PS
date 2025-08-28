f4c052c5a219503f864431ead97d5a13a4baed07
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-08-28 15:56:30 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 3844 ms, Memory: 37212 KB, Score: 37 point -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<long long int,int>> v[100001];
+long long int dist[100001];
+long long int pre[100001];
+int main() {
+  long long int N;
+  long long int M;
+  long long int A;
+  long long int B;
+  long long int C;
+  cin >> N >> M >> A >> B >>C;
+  for (long long int i=0; i<M; i++) {
+    long long int U,V,W;
+    cin >> U >> V >> W;
+    v[U].push_back(make_pair(W,V));
+    v[V].push_back(make_pair(W,U));
+  }
+  priority_queue<pair<long long int,long long int>,vector<pair<long long int,long long int>>,greater<pair<long long int,long long int>>>q;
+  q.push({0,A});
+  pre[B]=-1;
+  for (long long int i=1; i<=N; i++) {
+    pre[i]=1e18;
+  }
+  pre[A]=0;
+
+  while(!q.empty()) {
+    auto [cur_val,cur_node]=q.top();
+    q.pop();
+    //cout << cur_node << " " << cur_val << endl;
+
+    for (auto[nxt_val,nxt_node]: v[cur_node]) {
+      //cout << pre[nxt_node] << " " << pre[cur_node] << " " << nxt_val << endl;
+      if ( cur_val + nxt_val >C || (pre[nxt_node]<=max(pre[cur_node],nxt_val))){continue;}
+      pre[nxt_node]=max(pre[cur_node],nxt_val);
+      q.push({cur_val+nxt_val,nxt_node});
+    }
+
+  }
+  cout << pre[B];
+}
