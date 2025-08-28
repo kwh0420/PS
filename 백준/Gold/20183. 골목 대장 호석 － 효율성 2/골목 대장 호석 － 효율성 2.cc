ed0b3feb640fada27ec12a773298c70ab17ab559
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-08-28 16:14:59 +0900

    Update 골목 대장 호석 － 효율성 2.cc

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -34,11 +34,12 @@ int main() {
 
     for (auto[nxt_val,nxt_node]: v[cur_node]) {
       //cout << pre[nxt_node] << " " << pre[cur_node] << " " << nxt_val << endl;
-      if ( cur_val + nxt_val >C || (pre[nxt_node]<=max(pre[cur_node],nxt_val))){continue;}
+      if ( cur_val + nxt_val >C || (pre[nxt_node]<=max(pre[cur_node],nxt_val))){continue;} //다익스트라 알고리즘을 변형해서 최단거리가 아니더라도 우선순위큐에 들어올 수 있게 했음. 단, 현재 최대값보다 작아야함.
       pre[nxt_node]=max(pre[cur_node],nxt_val);
       q.push({cur_val+nxt_val,nxt_node});
     }
 
   }
   cout << ((pre[B]==1e18)?-1:pre[B]);
+
 }
