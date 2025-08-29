4c3216ed13c89764031aa067462464b7e1a766d2
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-08-29 10:08:50 +0900

    [Gold IV] Title: 세 수의 합, Time: 492 ms, Memory: 5228 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1002];
+vector<int> sum;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    int idx = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            sum.push_back( arr[i] + arr[j]);
+        }
+    }
+    sort(sum.begin(), sum.end());
+    int maxval = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        for (int j = 0; j < sum.size(); j++) {
+            if (arr[i] - sum[j] <= 0) { continue; }
+            if (binary_search(arr, arr + N, arr[i] - sum[j])) {
+                cout << arr[i];
+                return 0;
+            }
+        }
+    }
+    cout << maxval;
+}
