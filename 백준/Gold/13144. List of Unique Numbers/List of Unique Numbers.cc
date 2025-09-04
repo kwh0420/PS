922282dd4258d5aa5dfb80872c4a443b6d3d95e4
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-04 12:44:41 +0900

    Update List of Unique Numbers.cc

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -9,18 +9,19 @@ int main() {
         cin >> arr[i];
     }
     for (long long int i=0; i<100002; i++) {
-        has_table[i] = -1;
+        has_table[i] = -1;//해시테이블 -1로 초기화해주지 않으면 안됨
     }
     long long int st=0;
     long long int en=0;
     long long int count=0;
     while (en<n){
-        if (has_table[arr[en]]>=st) {
-            st=has_table[arr[en]]+1;
+        if (has_table[arr[en]]>=st) {//만약 범위 안에 중복수가 들어있을경우
+            st=has_table[arr[en]]+1; // 범위를 중복수 +1로 바꿈
         }
-        has_table[arr[en]]=en;
-        count+=en-st+1;
+        has_table[arr[en]]=en; 
+        count+=en-st+1; //count 계산, right-left+1;
         en++;
     }
     cout << count;
+
 }
