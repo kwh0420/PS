dd93a8a861fd28193dd6384283a11a6fefd7d561
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-08 14:22:54 +0900

    [Gold II] Title: 보석 도둑, Time: 264 ms, Memory: 24332 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -3,7 +3,7 @@ using namespace std;
 long long int n,k;
 multiset<long long int> s;
 long long int sum=0;
-vector<pair<long long int,long long int> > pq;
+priority_queue<pair<long long int,long long int> > pq;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -11,18 +11,17 @@ int main() {
     for(long long int i=0; i<n; i++) {
         long long int q,r;
         cin>>q >>r;
-        pq.push_back(make_pair(r,q));
+        pq.push(make_pair(r,q));
     }
-    sort(pq.begin(),pq.end(),greater<pair<long long int,long long int> >());
     for (long long int i=0; i<k;i++) {
         long long int g;
         cin>>g;
         s.insert(g);
     }
-    long long int idx=0;
-    for (int idx=0; idx<n; idx++) {
-    auto[v,w]  =pq[idx];
-        auto it =s.lower_bound(w);
+    while (!pq.empty()) {
+        auto[v,w]  =pq.top();
+        pq.pop();
+        auto it = s.lower_bound(w);
         if (s.end()!=it) {
             sum+=v;
             s.erase(it);
