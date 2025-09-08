ddacf5edd5f7b83475c615714c0eed7003bf785f
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-08 18:54:34 +0900

    [Gold I] Title: 수업, Time: 236 ms, Memory: 10864 KB -BaekjoonHub

---
+++ 백준/Gold/19700. 수업/수업.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+priority_queue<pair<int,int>> pq;
+multiset<int> s;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    int K=N;
+    while (N--) {
+        int height,cnt;
+        cin >> height >> cnt;
+        pq.push(make_pair(height,cnt));
+    }
+    while (K--) {
+        auto[height,cnt] = pq.top();
+        pq.pop();
+        auto it= s.lower_bound(cnt);
+        if (it == s.begin()) {
+            s.insert(1);
+        }
+        else {
+            it--;
+            s.insert((*it)+1);
+            s.erase(it);
+        }
+    }
+    cout << s.size();
+}
