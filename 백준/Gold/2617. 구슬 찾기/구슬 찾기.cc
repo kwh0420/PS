92e0de1dd066d3a1f6de194d3cf8e4cb909ae645
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-11 15:04:46 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2428 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -1,76 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-int num[102][2];
-vector<int> v1[102];
-vector<int> v2[102];
-set<pair<int,int>>ss;
-void bfs1(int i) {
-    int vis[102]={0};
-    queue<int> q;
-    q.push(i);
-    vis[i]=1;
-    int cnt=0;
-    while(!q.empty()) {
-        int u=q.front();
-        q.pop();
-        for (auto j : v1[u]) {
-            if(vis[j]==0) {
-                cnt++;
-                q.push(j);
-                vis[j]=1;
-            }
-        }
+
+int num[102][2];       // [i][0] = i보다 큰 구슬 수, [i][1] = i보다 작은 구슬 수
+vector<int> v1[102];   // i보다 큰 구슬 그래프
+vector<int> v2[102];   // i보다 작은 구슬 그래프
+set<pair<int,int>> ss;
+
+int dfs1(int i, vector<int>& vis) {
+    vis[i] = 1;
+    int cnt = 0;
+    for (auto j : v1[i]) {
+        if (!vis[j]) cnt += dfs1(j, vis) + 1;
     }
-    num[i][0]=cnt;
+    return cnt;
 }
-void bfs2(int i) {
-    int vis[102]={0};
-    queue<int> q;
-    q.push(i);
-    vis[i]=1;
-    int cnt=0;
-    while(!q.empty()) {
-        int u=q.front();
-        q.pop();
-        for (auto j : v2[u]) {
-            if(vis[j]==0) {
-                cnt++;
-                q.push(j);
-                vis[j]=1;
-            }
-        }
+
+int dfs2(int i, vector<int>& vis) {
+    vis[i] = 1;
+    int cnt = 0;
+    for (auto j : v2[i]) {
+        if (!vis[j]) cnt += dfs2(j, vis) + 1;
     }
-    num[i][1]=cnt;
+    return cnt;
 }
+
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
-    int n,m;
-    cin>>n>>m;
-    for (int i=1; i<=n; i++) {
-        num[i][0]=-1;
-        num[i][1]=-1;
-    }
-    for(int i=0;i<m;i++) {
-        int a,b;
-        cin>>a>>b;
-        if (ss.find({a,b})!=ss.end()) continue;
-        v1[a].push_back(b);
-        v2[b].push_back(a);
+    int n, m;
+    cin >> n >> m;
+
+    for (int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (ss.count({a,b})) continue;  // 중복 간선 방지
+        v1[a].push_back(b);  // a보다 큰 b
+        v2[b].push_back(a);  // b보다 작은 a
         ss.insert({a,b});
     }
+
     for (int i=1; i<=n; i++) {
-        bfs1(i);
-        bfs2(i);
+        vector<int> vis1(n+1, 0);
+        num[i][0] = dfs1(i, vis1);
+
+        vector<int> vis2(n+1, 0);
+        num[i][1] = dfs2(i, vis2);
     }
+
     int limit = (n-1)/2;
-    int cnt=0;
-    for(int i=1;i<=n;i++) {
-        if (limit < num[i][0] || limit < num[i][1]) {
-            cnt++;
-        }
+    int cnt = 0;
+    for (int i=1; i<=n; i++) {
+        if (num[i][0] > limit || num[i][1] > limit) cnt++;
     }
     cout << cnt;
 }
-
