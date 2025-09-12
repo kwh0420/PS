9df49741320ff31cead9d966a539468e7a7effc4
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-12 11:57:43 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 36 ms, Memory: 9228 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v[100002];
+int vis[100002];
+int par[100002];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N,M;
+    cin>>N>>M;
+    for(int i=0;i<M;i++) {
+        int a,b;
+        cin>>a>>b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    int ans=0;
+    int cnt=0;
+    int result=0;
+    for(int i=1;i<=N;i++) {
+        if (vis[i] == 1){continue;}
+        cnt++;
+        queue<int> q;
+        q.push(i);
+        vis[i] = 1;
+        while(!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for(int j=0;j<v[cur].size();j++) {
+                if(vis[v[cur][j]] == 1) {
+                    if (v[cur][j]!=par[cur])
+                    {ans++;}
+                }
+                else{
+                    q.push(v[cur][j]);
+                    vis[v[cur][j]] = 1;
+                    par[v[cur][j]] = cur;
+                }
+            }
+        }
+    }cout<<(ans/2)+cnt-1;
+
+}
