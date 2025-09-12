158a86b3a3420523b5094f2a723c4fdd8575c355
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-12 14:44:43 +0900

    [Gold IV] Title: 회사 문화 1, Time: 92 ms, Memory: 8184 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v[100002];
+int par[100002];
+int result[100002];
+void dfs(int cur, int par) {
+    for (auto i : v[cur]) {
+        if (i == par) {continue;}
+        result[i]+=result[cur];
+    }
+}
+int main() {
+    int n,m;
+    cin>>n>>m;
+    for(int i=1; i<=n; i++) {
+        int a;
+        cin>>a;
+        if (a==-1){continue;}
+        v[i].push_back(a);
+        v[a].push_back(i);
+        par[i]=a;
+    }
+    for(int i=1; i<=m; i++) {
+        int a,b;
+        cin>>a>>b;
+        result[a]+=b;
+    }
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        int cur=q.front();
+        q.pop();
+        for (auto i : v[cur]) {
+            if (i==par[cur])continue;
+            result[i]+=result[cur];
+            q.push(i);
+        }
+    }
+    for(int i=1; i<=n; i++) {
+        cout<<result[i]<<" ";
+    }
+}
