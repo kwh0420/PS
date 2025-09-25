8e8aea7b3177484acfe6508c4b26c345f4b0a0a0
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-25 10:24:42 +0900

    Update 오큰수.cc

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-stack<pair<int,int>>pq;
+stack<pair<int,int>>pq; // pq를 우선순위 큐로 해야된다고 생각했다. 그런데 top>arr[i]를 유지하므로  pq는 최상단 수를 기준으로 오름차순으로 정렬된다. 따라서 스택으로 두어도 상관이 없다. (스택-> 우선순위큐 280ms -> 180ms)
 int N;
 int arr[1000002];
 int result[1000002];
@@ -25,3 +25,4 @@ int main() {
         cout << result[i] << " ";
     }
 }
+
