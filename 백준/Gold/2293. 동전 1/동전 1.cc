80bfb68ef291a1608a10beb6f69d76fc79a63c84
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-25 11:00:15 +0900

    Update 동전 1.cc

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -15,8 +15,9 @@ int main() {
         dp[coin[idx]]+=1;
         for (int v = 0; v <= k; v++) {
             if (v-coin[idx]>=0)
-                dp[v] += dp[v-coin[idx]];
+                dp[v] += dp[v-coin[idx]]; // 2차원 dp가아닌 1차원 dp로 풀어야함. 동전을 크기순으로 정렬한 후, 초기값으로 1을 준다. 10000*100 = 약4mb로 문제조건에 아슬아슬하기때문
         }
     }
     cout << dp[k];
+
 }
