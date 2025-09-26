034cfa028511a6a8c7a44b988e1e028bdfc4cdb7
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-09-26 13:52:54 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,w,L;
-int truck_weight[1002];
-int indexindex=0;
-int weight_sum=0;
-queue<pair<int,int>> q;
+int truck[1002];
+int weightsum=0;
+int idx=0;
 int main() {
-    cin >> n >> L >> w;
-    for (int i = 0; i < n; i++) {
-        cin >> truck_weight[i];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,L,w;
+    cin>>n>>w>>L;
+    queue<int> q;
+    for(int i=0;i<w;i++) {
+        q.push(0);
+    }
+    for (int i=0; i<n; i++) {
+        cin >> truck[i];
     }
     int time=0;
-    q.push({0,truck_weight[0]});
-    weight_sum+=truck_weight[indexindex++];
-    while (!q.empty()) {
-        time++;
+    while(!q.empty()) {
+        time ++;
+        weightsum-=q.front();
+        q.pop();
+        if (idx<n&&truck[idx]+weightsum<=L) {
+            weightsum+=truck[idx];
+            q.push(truck[idx]);
 
-        
-        if (time-q.front().first>=L) {
-            weight_sum-=q.front().second;
-            q.pop();
+            idx++;
         }
-        if (indexindex == n) {
-            continue;
-        }
-        if (q.size()<L) {
-            if (weight_sum+truck_weight[indexindex]<=w) {
-                q.push({time,truck_weight[indexindex]});
-                weight_sum+=truck_weight[indexindex];
-                indexindex ++;
-            }
+        else if (idx<n){
+            q.push(0);
         }
     }
-    cout <<time+1;
+    cout << time << "\n";
 }
