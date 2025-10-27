01cee97e6203216d4ab843342145acd1ba8066da
kwh0420 <143983378+kwh0420@users.noreply.github.com>
2025-10-27 13:23:11 +0900

    [Gold I] Title: 청소년 상어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.cc
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxresult = 0;
+int result = 0;
+int numofish[4][4];
+int dirofish[4][4];
+pair<int, int> locofshark;
+int row_dir[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int col_dir[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+pair<int,int> findloc(int num) {
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
+            if (numofish[i][j] == num) {
+                return make_pair(i, j);
+            }
+        }
+    }
+    return {-1,-1};
+}
+void printx() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << numofish[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << dirofish[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "------------\n";
+}
+
+void shift(int i, int j) {
+    int num = numofish[i][j];
+    if (num == 0 || num == -1) { return; }
+    for (int k = 0; k < 8; k++) {
+        int cur_dir = dirofish[i][j];
+
+        int nrow = i + row_dir[cur_dir];
+        int ncol = j + col_dir[cur_dir];
+        if (0 > nrow || 0 > ncol || nrow >= 4 || ncol >= 4 || numofish[nrow][ncol] == -1) {
+            dirofish[i][j] = (dirofish[i][j] + 1) % 8;
+            continue;
+        } else {
+            swap(numofish[nrow][ncol], numofish[i][j]);
+            swap(dirofish[nrow][ncol], dirofish[i][j]);
+            return;
+        }
+    }
+    return;
+}
+
+void func(int depth) {
+    int temp[4][4]={0};
+    int temp2[4][4]={0};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            temp[i][j] = dirofish[i][j];
+            temp2[i][j] = numofish[i][j];
+        }
+    }
+    for (int i=1; i<=16; i++) {
+        auto [row,col] = findloc(i);
+        if (row==-1 || col==-1) { continue; }
+        shift(row, col);
+    }
+    //printx();
+
+    int cur_row = locofshark.first;
+    int cur_col = locofshark.second;
+    for (int i = 1; i < 4; i++) {
+        int nrow = row_dir[dirofish[cur_row][cur_col]] * i + cur_row;
+        int ncol = col_dir[dirofish[cur_row][cur_col]] * i + cur_col;
+        if (0 > nrow || 0 > ncol || nrow >= 4 || ncol >= 4) { continue; }
+        if (numofish[nrow][ncol] == 0) { continue; }
+        int t = numofish[nrow][ncol];
+        int p = dirofish[cur_row][cur_col];
+        numofish[nrow][ncol] = -1;
+        dirofish[cur_row][cur_col] = 0;
+        numofish[cur_row][cur_col] = 0;
+        locofshark= {nrow, ncol};
+        result += t;
+        func(depth + 1);
+        numofish[nrow][ncol] = t;
+        dirofish[cur_row][cur_col] = p;
+        numofish[cur_row][cur_col] = -1;
+        locofshark = {cur_row, cur_col};
+        result -=t;
+    }
+    maxresult = max(result, maxresult);
+    swap(temp,dirofish);
+    swap(temp2,numofish);
+}
+
+int main() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> numofish[i][j] >> dirofish[i][j];
+            dirofish[i][j]--;
+            if (i == 0 && j == 0) {
+                result += numofish[i][j];
+                numofish[i][j] = -1;
+            }
+        }
+    }
+    locofshark = {0, 0};
+    func(0);
+    cout << maxresult ;
+}
